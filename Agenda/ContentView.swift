3db15e1c48d3be482daacc70da11f561b8a51fde
Carl Weis <carl@givecampus.com>
2019-06-13 19:52:07 -0400

    Initial Commit

---
+++ Agenda/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Agenda
+//
+//  Created by Carl Weis on 6/13/19.
+//  Copyright © 2019 Carl Weis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
