1264f45d4c044e51c6ea17462d08dd7c93e8cc4c
Kari <kj7rosebud@gmail.com>
2019-06-13 21:55:05 -0400

    add agenda model

---
+++ Agenda/Models/Agneda.swift
@@ -0,0 +1,15 @@
+//
+//  Agneda.swift
+//  Agenda
+//
+//  Created by Karen Weis on 6/13/19.
+//  Copyright © 2019 Carl Weis. All rights reserved.
+//
+
+import SwiftUI
+
+struct Agenda: Identifiable {
+  var id: UUID = UUID ()
+  var name: String
+  var icon: String?
+}
